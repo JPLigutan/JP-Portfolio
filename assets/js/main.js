b39817f5f8b03d9db227a13f997241f0f412fd11
JPLigutan <jeanpaulligutan@gmail.com>
2022-06-11 01:49:38 +0800

    initialcommit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,13 @@
 $(window).on("load", function() {
     var t = $("#sub_1")
         , t2 = $("#sub_2")
-        , t3 = $("#sub_3");
+        , t3 = $("#sub_3")
+        , tx=t.text()
+        , tx=t2.text()
+        , t3x=t3.text();
     $(".section_cls .container_cust");
     function animate_type() {
-        var i=0,tx=t.text();function typeWriter(){t.addClass("active"),i<tx.length&&(document.getElementById("sub_1").innerHTML+=tx.charAt(i),i++,setTimeout(typeWriter,200))}t.empty(),setTimeout(function(){typeWriter()},1200);var i2=0,t2x=t2.text();function typeWriter2(){t2.addClass("active"),i2<t2x.length&&(document.getElementById("sub_2").innerHTML+=t2x.charAt(i2),i2++,setTimeout(typeWriter2,200))}t2.empty(),setTimeout(function(){typeWriter2()},3e3);var i3=0,t3x=t3.text();function typeWriter3(){t3.addClass("active"),i3<t3x.length&&(document.getElementById("sub_3").innerHTML+=t3x.charAt(i3),i3++,setTimeout(typeWriter3,200))}$("#sub_3").empty(),setTimeout(function(){typeWriter3()},4800)
+        var i=0;function typeWriter(){t.addClass("active"),i<tx.length&&(document.getElementById("sub_1").innerHTML+=tx.charAt(i),i++,setTimeout(typeWriter,200))}t.empty(),setTimeout(function(){typeWriter()},1200);var i2=0;function typeWriter2(){t2.addClass("active"),i2<t2x.length&&(document.getElementById("sub_2").innerHTML+=t2x.charAt(i2),i2++,setTimeout(typeWriter2,200))}t2.empty(),setTimeout(function(){typeWriter2()},3e3);var i3=0;function typeWriter3(){t3.addClass("active"),i3<t3x.length&&(document.getElementById("sub_3").innerHTML+=t3x.charAt(i3),i3++,setTimeout(typeWriter3,200))}$("#sub_3").empty(),setTimeout(function(){typeWriter3()},4800)
         $("#hero_imgs .img_res").each(function(e) {
             var k = $(this);
             s = 500 + (e * 750)
